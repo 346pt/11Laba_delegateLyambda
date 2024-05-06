41e69543cd8a22f29d42b4444a4407b69649accf
obi1anzor075 <112080391+obi1anzor075@users.noreply.github.com>
2024-05-06 17:24:46 +0300

    Добавьте файлы проекта.

---
+++ 11Laba_delegateLyambda/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+delegate string DayOfWeekDelegate();
+class Program {
+    static void Main(string[] args) {
+
+        string[] Days = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+        int currentIndex = 0;
+
+        DayOfWeekDelegate getDay = () =>
+        {
+            string day = Days[currentIndex];
+            currentIndex = (currentIndex + 1) % Days.Length;
+            return day;
+        };
+
+        for (int i = 0; i < 7; i++)
+        {
+            Console.WriteLine(getDay());
+        }
+        Console.ReadKey();
+    }
+}
